a152498fb6b856ac09df6d22585c56e6ed8ce412
Joshua Olson <joshua.olson@gmail.com>
2011-04-20 17:34:26 -0700

    Moved to using the email package.

--- dataset.py
+++ dataset.py
@@ -17,10 +17,11 @@ Optional: Inside the home directory there is an ignore list of words to discard
 """
 
 import sys, os
-import random, string
+import random, string, email, re
 import nltk, nltk.corpus
 
 class DataMember:
+    """Defines a line in the dataset"""
     def __init__(self):
         self.ip_address = ''
         self.degree_domains_match = 0.0
@@ -36,9 +37,8 @@ class DataMember:
         self.degree_spam = 0.0
         #False for ham, True for spam
         self.spam = False
-
-    def _print(self, str):
-        print str % \
+    def _print(self, format):
+        return format % \
             (self.ip_address,           \
             self.degree_domains_match,  \
             self.subject,               \
@@ -50,29 +50,128 @@ class DataMember:
             self.percent_spam,          \
             self.degree_spam,           \
             self.spam)    
-        
-    def printFile(self):
-        str = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s"
-        self._print(str)
-        
-        
-    def println(self):
-        print "New entry::"
-        str = "%s|||%s|||%s|||%s|||%s|||%s|||%s|||%s|||%s|||%s|||%s"
-        self._print(str)
-        print ''
-        
+    def stringFile(self):
+        format = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s'
+        return self._print(format)
+    def stringPrint(self):
+        format = '( %s|||%s|||%s|||%s|||%s|||%s|||%s|||%s|||%s|||%s|||%s )'
+        return self._print(format)        
+
+
+def get_message_body(mail):
+    text = ''
+    if not mail.is_multipart():
+        text = re.sub('(=?[\\r\\n])|(=[\\da-fA-F]{2})','', mail.get_payload())
+    else:
+        for part in mail.get_payload():
+            attachment = False
+            for key in part.keys():
+                #Check all keys for an attachment name
+                if key.startswith('Content'):
+                    if str(part[key]).find('name') != -1:
+                        attachment = True
+            #Only check get the body of the next part if it's not an attachment
+            if not attachment:
+                text += get_message_body(part) + ' '  
+    return text
+
+
+def get_message_len(mail):
+    text = 0 #Will be off by one, but this prevents /0 errors
+    if not mail.is_multipart():
+        text = len(mail.get_payload())
+    else:
+        for part in mail.get_payload():
+            attachment = False
+            for key in part.keys():
+                #Check all keys for an attachment name
+                if key.startswith('Content'):
+                    if str(part[key]).find('name') > -1:
+                        attachment = True
+            #Only check the len of the next part if it's not an attachment
+            if not attachment:                    
+                text += get_message_len(part)
+    return max(text,1)    
+
+
+def get_type_content(mail):
+    """Return values
+    text based = 1
+    HTML based = 2
+    mixed = 3"""
+    type = 1
+    if not mail.is_multipart():
+        #Contains an attachment
+        for key in mail.keys():
+            #Check all keys for an attachment file"name"
+            if key.startswith('Content'):
+                if str(mail[key]).find('name') == -1:
+                    if str(mail[key]).startswith('text/html'):
+                        if type <= 2:
+                            type = 2
+                        else:
+                            type = 3
+                    elif str(mail[key]).startswith('text/plain'):: 
+                        if type == 2:
+                            type = 3
+        return type
+    else:
+        for part in mail.get_payload():
+            temp = get_type_content(part)
+            if (type > 1 and type != temp):
+                return 4
+            else:
+                type = temp
+    return type
+
+
+def get_type_attachments(mail):
+    """Return values
+    No attachments = 1
+    Text attachments only = 2
+    Non-text attachments only = 3
+    Mixed attachments = 4"""
+    type = 1
+    if not mail.is_multipart():
+        #Contains an attachment
+        for key in mail.keys():
+            #Check all keys for an attachment name
+            if key.startswith('Content'):
+                if str(mail[key]).find('name') != -1:
+                    if str(mail[key]).startswith('text'):
+                        if type <= 2:
+                            type = 2
+                        else:
+                            type = 4
+                    else: #Non-text attachment
+                        if type % 2: 
+                            type = 3
+                        else:
+                            type = 4
+        return type
+    else:
+        for part in mail.get_payload():
+            temp = get_type_attachments(part)
+            if temp > 3 or (type > 1 and type != temp):
+                return 4
+            else:
+                type = temp
+    return type
+
 
 def word_count_dict(home_dir, dir, percent):
     """Returns a word/count dict for this directory."""
     word_count = {}
     file_list = os.listdir(home_dir + dir)
     count = len(file_list)
-    numSamples = max(1, int(count * percent))
+    num_samples = max(1, int(count * percent))
     sample_files = []
+    
     #Sample x% of all files to build this thesaurus 
-    for n in range(numSamples):
+    for n in range(num_samples):
         sample_files.append( file_list[ int(random.uniform(0,count)) ] )
+        
+    #Load the list of words to ignore (optional)
     if os.path.isfile(home_dir + 'ignore.txt'):
         ignore_file = open(home_dir + 'ignore.txt', 'r')
         ignore = nltk.word_tokenize(ignore_file.read())
@@ -80,17 +179,17 @@ def word_count_dict(home_dir, dir, percent):
     else:
         ignore_file = []
         
-    for filename in sample_files:
-        if filename[0] == '.':
+    for file_name in sample_files:
+        #Ignore files that start with .
+        if file_name[0] == '.':
             continue
-        input_file = open(home_dir + dir + '/' + filename, 'r')
-        text = input_file.read()
-        subjectIndex = text.find("Subject: ")
-        text = text[subjectIndex:]
-        #Clean the message of attachments
-        attachmentIndex = text.find("Content-Disposition: attachment")
-        if attachmentIndex > 0:
-            text = text[:attachmentIndex]
+        input_file = open(home_dir + dir + '/' + file_name, 'r')
+        mail = email.message_from_file(input_file)
+        input_file.close()
+        
+        #Get the message contents
+        text = get_message_body(mail)
+            
         raw = nltk.clean_html(text)
         words = nltk.wordpunct_tokenize(raw.lower())
         for word in words:
@@ -102,7 +201,6 @@ def word_count_dict(home_dir, dir, percent):
                         word_count[word] = 1
                 else:
                     word_count[word] = word_count[word] + 1
-        input_file.close()  # Not strictly required, but good form.
         
     return word_count
 
@@ -117,7 +215,6 @@ def build_thesaurus(home_dir, dir, percent):
     #Sort words based on the frequency of the word
     count = 0
     for word in sorted(word_count, key = word_count.get, reverse = True):
-        #print word, word_count[word]
         file.write(word + ' ' + str(word_count[word]) + '\n')
         if count < len(word_count) / 2:
             file2.write(word + ' ' + str(word_count[word]) + '\n')
@@ -128,7 +225,7 @@ def build_thesaurus(home_dir, dir, percent):
     file.close()
     file2.close()
     return word_count, top_50
-    
+
 
 def spam_unique(home_dir, ham, spam):
     unique = {}
@@ -138,57 +235,80 @@ def spam_unique(home_dir, ham, spam):
             unique[word] = spam[word]
             file.write(word + ' ' + str(spam[word]) + '\n')        
     file.close()
-    
-    
+
+
 def build_dataset(home_dir, dir, spam, spam_top_50):
     """Build dataset
     Data set should have the fields
     1.) IP Address from the received field in the header
-    2.) Matching degree of domain names between Message-Id and (Received/From ??) field (Easy just read and compare)
-    3.) Subject (Easy just read it)
-    4.) Name from the From field (Easy just read it)
-    5.) Content type (Easy just read it)
-    6.) Attachments: none, text, or non-text (Need to parse this)
-    7.) Number of URLs present (Count http:// links)
-    8.) URL ratio (% of message body that is URLs) (This one is tricky, how much of the message to use?)
-    9.) SPAM word ratio (Again how much of the message to use? Exclude header and attachments?)
-    10.) SPAM degree as by equation in paper (Simple calculation)
-    11.) Classification label: Spam or Ham (What the classification SHOULD be. Predefined by filename)"""
+    2.) Matching degree of domain names between Message-Id and Received/From
+    3.) Subject 
+    4.) Name from the From field 
+    5.) Content type
+    6.) Attachments: none, text, or non-text 
+    7.) Number of URLs present 
+    8.) URL ratio 
+    9.) SPAM word ratio 
+    10.) SPAM degree as by equation in paper 
+    11.) Classification label: Spam or Ham """
+    
     file_list = os.listdir(home_dir + dir)
     count = len(file_list)
     data_set = {}
     for file_name in file_list:
         data_set[file_name] = DataMember()
+        #Ignore files that start with .
         if file_name[0] == '.':
             continue 
+        #print file_name    
         file = open(home_dir + dir + '/' + file_name)
-        for line in file:
+        mail = email.message_from_file(file)
+        file.close()
+        
+        #Extract information from header
+        for key in mail.keys():
             #1.) IP Address from the received field in the header (Easy just read it)
             #Get the IP address of the last Received from field unless its 127.0.0.1
-            if line.startswith('Received: from'):
-                start = line.find('[') + 1
-                end = line.find(']') 
-                if start < end and line[start:end].find('127.0.0.1') == -1:
-                    data_set[file_name].ip_address = line[start:end]
-                    colon = data_set[file_name].ip_address.find(':')
-                    if colon != -1:
-                        data_set[file_name].ip_address = data_set[file_name].ip_address[:colon]
+            if key == 'Received':
+                address = re.search('(\d{1,3}\.){3}\d{1,3}',mail[key]).group()
+                if address != '127.0.0.1':
+                    data_set[file_name].ip_address += ' ' + address
+                        
+            #2.) Matching degree of domain names between Message-Id and (Received/From ??) field (Easy just read and compare)
+                        
             #3.) Subject (Easy just read it)          
-            if line.startswith('Subject: '):
-                data_set[file_name].subject = line[9:-1]
-                #print file_name
-                #print data_set[file_name].subject
-
-            #3.) Subject (Easy just read it)          
-            if line.find('http://') != -1:
-                if line.find('schema') == -1 and line.find('Schema') == -1:
-                    print line
+            if key == 'Subject':
+                data_set[file_name].subject = mail[key]
                 
-        #print data_set[file_name].println()
-        file.close()
+            #4.) Name from the From field (Easy just read it)
+            if key == 'From':
+                data_set[file_name].name = mail[key]
+            
+            #5.) Content type (Easy just read it)
+            
+                
+            #9.) SPAM word ratio 
+            #10.) SPAM degree as by equation in paper 
+            
+        #6.) Attachments: none, text, or non-text 
+        data_set[file_name].attachments = get_type_attachments(mail)
+        #7.) Number of URLs present 
+        data_set[file_name].num_urls = len( re.findall( \
+                    'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', \
+                    get_message_body(mail)) )
+        #8.) URL ratio (% of message body that is URLs)
+        length = (get_message_len(mail) * 1.0)
+        data_set[file_name].percent_urls = data_set[file_name].num_urls / length
+        #11.) Classification label: Spam or Ham
+        if file_name.startswith('spam'):
+            data_set[file_name].spam = True
+      
+        #print data_set[file_name].stringPrint()
+    for key in data_set.keys():
+        print data_set[key].stringPrint()
     #Repeat for ham files
-    
-    
+
+
 def main():
     if len(sys.argv) != 4:
         print 'usage: ./dataset.py ham_dir spam_dir %_to_sample'
