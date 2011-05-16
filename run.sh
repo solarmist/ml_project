5b70f48eee45865d57d1757f42ec023780e0d693
Joshua Olson <joshua.olson@gmail.com>
2011-05-16 11:14:29 -0700

    Added building the dataset and splitting the files.

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+../dataset.py ./ ham spam 30
+../split.py
+
 java weka.classifiers.trees.J48 -t set12.arff -T test3.arff>results1.txt
 java weka.classifiers.trees.J48 -t set13.arff -T test2.arff>results2.txt
 java weka.classifiers.trees.J48 -t set23.arff -T test1.arff>results3.txt
